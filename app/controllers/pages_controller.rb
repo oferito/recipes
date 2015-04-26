f9d2b7aea3a0869f67e1cdcb14649e875339d3fd
Ofer Lehr <ofer@oferlehr.com>
2015-04-26 20:36:42 +0300

    Created pages controller and set up routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,27 @@
+class PagesController < ApplicationController
+
+	def home
+	end
+
+	def index
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def edit
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+
+end
